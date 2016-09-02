fb0e3b5588e07855deb2d0dbcd6f2579727f452b
midasmn <midasmn@outlook.com>
2016-09-03 02:26:27 +0900

    Delete mysql-ini.php

--- lib/mysql-ini.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "mysql1005.xserver.jp";   // 接続するMySQLサーバー
-$user = "midasmn_admin";      // MySQLのユーザー名
-$pass = "nBxYzMxX47u";      // MySQLのパスワード
-$dbname= "midasmn_calendar";      // DBの名前
-?>
